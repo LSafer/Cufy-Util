84d7452e17b7cc5ba2fbecac2f686acdf6c9e5db
LSafer <lsaferse@gmail.com>
2020-02-16 22:01:13 +0300

    Update Object$.java
    - added remoteMap(Object, boolean):
        Created to be able to get a map that uses the object's fields as it's entries (whatever the type of that object)

--- src/main/java/cufy/util/Object$.java
+++ src/main/java/cufy/util/Object$.java
@@ -10,7 +10,8 @@
  */
 package cufy.util;
 
-import java.util.Objects;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.function.Supplier;
 
 /**
@@ -31,6 +32,78 @@ final public class Object$ {
 	}
 
 	/**
+	 * Get a map that whenever the method {@link Map#put(Object, Object)} is invoked to it. It will put the value to an existing PUBLIC NON-FINALE
+	 * field with the name of the key's value (only if it is string). And whenever the method {@link Map#get(Object)} is invoked to it. It will get
+	 * the value of an existing PUBLIC field.
+	 *
+	 * @param object     the instance
+	 * @param restricted if it is allowed to throw exception
+	 * @return a remote map to that instance
+	 * @apiNote the returned map WILL contain ALL the accessible fields of the given instance! so be careful when using it!.
+	 */
+	public static Map<String, Object> remoteMap(Object object, boolean restricted) {
+		Objects.requireNonNull(object, "object");
+		return new AbstractMap<String, Object>() {
+			/**
+			 * The entry-set of this map.
+			 */
+			private Set<Map.Entry<String, Object>> entrySet;
+
+			@Override
+			public Object put(String key, Object value) {
+				for (Entry<String, Object> entry : this.entrySet())
+					if (entry.getKey().equals(key))
+						return entry.setValue(value);
+
+				if (restricted)
+					throw new UnsupportedOperationException();
+
+				return null;
+			}
+			@Override
+			public Set<Entry<String, Object>> entrySet() {
+				if (this.entrySet == null) {
+					HashSet tempSet = new HashSet();
+					for (Field field : Reflect$.getAllFields(object.getClass()))
+						tempSet.add(new Entry<String, Object>() {
+							@Override
+							public String getKey() {
+								return field.getName();
+							}
+							@Override
+							public Object getValue() {
+								try {
+									field.setAccessible(true);
+									return field.get(object);
+								} catch (Throwable e) {
+									if (restricted)
+										throw new UnsupportedOperationException(e);
+								}
+
+								return null;
+							}
+							@Override
+							public Object setValue(Object value) {
+								try {
+									Object old = this.getValue();
+									field.set(object, value);
+									return old;
+								} catch (Throwable e) {
+									if (restricted)
+										throw new UnsupportedOperationException(e);
+								}
+								return null;
+							}
+						});
+					this.entrySet = Collections.unmodifiableSet(tempSet);
+				}
+
+				return this.entrySet;
+			}
+		};
+	}
+
+	/**
 	 * Returns the first argument if it is non-null and otherwise returns the non-null second argument. Note: this is a copy/paste from {@link
 	 * java.util.Objects#requireNonNullElseGet(Object, Supplier)}. For lower java-lang levels.
 	 *
@@ -44,7 +117,6 @@ final public class Object$ {
 	public static <T> T requireNonNullElse(T obj, T defaultObj) {
 		return obj != null ? obj : Objects.requireNonNull(defaultObj, "defaultObj");
 	}
-
 	/**
 	 * Returns the first argument if it is non-null and otherwise returns the non-null value of supplier.get(). Note: this is a copy/paste from {@link
 	 * java.util.Objects#requireNonNullElse(Object, Object)} (Object, Object)}. For lower java-lang levels.
