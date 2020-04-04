ce198f4180dcfa519bfb97249a6c4e6d95047149
LSafer <lsaferse@gmail.com>
2020-04-04 08:00:22 +0300

    moved 'Object$.remoteMap()' to Map$.java
    it fits there better

--- src/test/java/cufy/util/Object$Test.java
+++ src/test/java/cufy/util/Object$Test.java
@@ -14,73 +14,8 @@ package cufy.util;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Map;
-
 @SuppressWarnings("JavaDoc")
 public class Object$Test {
-	@Test
-	public void remoteMap() {
-		class TestObject {
-			final public int fin = 5;
-			public int pub = 3;
-			protected int pro = 8;
-			int pac = 4;
-			private int pri = 11;
-		}
-
-		TestObject instance = new TestObject();
-		Map<String, Integer> remote = (Map<String, Integer>) (Map) Object$.remoteMap(instance, true);
-
-		Assert.assertEquals("Can't get public fields", (Integer) 3, remote.get("pub"));
-		Assert.assertEquals("Can't get package-private fields", (Integer) 4, remote.get("pac"));
-		Assert.assertEquals("Can't get protected fields", (Integer) 8, remote.get("pro"));
-		Assert.assertEquals("Can't get private fields", (Integer) 11, remote.get("pri"));
-
-		remote.put("pub", 20);
-		remote.put("pro", 30);
-		remote.put("pac", 40);
-		remote.put("pri", 50);
-
-		Assert.assertEquals("Can't replace public fields", (Integer) 20, remote.get("pub"));
-		Assert.assertEquals("Can't replace package-private fields", (Integer) 30, remote.get("pro"));
-		Assert.assertEquals("Can't replace protected fields", (Integer) 40, remote.get("pac"));
-		Assert.assertEquals("Can't replace private fields", (Integer) 50, remote.get("pri"));
-
-		try {
-			remote.remove("pac");
-			Assert.fail("Remove should not work");
-		} catch (UnsupportedOperationException ignored) {
-		}
-		try {
-			remote.clear();
-			Assert.fail("Clear should not work");
-		} catch (UnsupportedOperationException ignored) {
-		}
-		try {
-			remote.put("x", 1);
-			Assert.fail("No such field!");
-		} catch (UnsupportedOperationException ignored) {
-		}
-		try {
-			((Map) remote).put("c", "ABCDEF");
-			Assert.fail("ClassCastException is expected");
-		} catch (UnsupportedOperationException ignored) {
-		}
-
-		HashSet<Integer> set = new HashSet<>();
-
-		((Map) remote).forEach((k, v) -> {
-			//this$0 and other arguments :)
-			if (v instanceof Integer)
-				set.add((int) v);
-		});
-
-		Assert.assertTrue("Missing value", set.contains(20));
-		Assert.assertTrue("Missing value", set.contains(30));
-		Assert.assertTrue("Missing value", set.contains(40));
-		Assert.assertTrue("Missing value", set.contains(50));
-	}
 	@Test
 	public void requireNotNullElse() {
 		Object o = Object$.requireNonNullElse('O', 'D');
